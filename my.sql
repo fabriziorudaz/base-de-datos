99dfa83723198759e30274d183ea7ddcb4e07648
fabriziorudaz <130599313+fabriziorudaz@users.noreply.github.com>
2023-08-30 12:13:59 -0300

    Update my.sql

--- my.sql
+++ my.sql
@@ -10,25 +10,46 @@ create table articulos(
     cod_art int(5) not null AUTO_INCREMENT primary key,
     nombre varchar(20) not null,
     precio int(7) not null,
-    fabricante int(5),
+    fabricante int(5) ,
     FOREIGN key(fabricante)references fabricantes(cod_fab) 
 );
 
 insert into fabricantes(cod_fab, nombre) values
-(null, 'juan'),
-(null, 'pedro'),
-(null, 'jaimito'),
-(null, 'fede'),
-(null, 'gerardo');
+(null, 'samsung'),
+(null, 'apple'),
+(null, 'motorola'),
+(null, 'hp'),
+(null, 'dell');
 
-insert into articulos(cod_art, nombre, precio) values
-(null, 'monintor', '100000'),
-(null, 'pc', '120000'),
-(null, 'mouse', '12500'),
-(null, 'router', '45600'),
-(null, 'celular', '110000' ),
-(null, 'teclado', '20000'),
-(null, 'motherboard', '56000'),
-(null, 'disco_duro', '43000'),
-(null, 'auriculares', '14300'),
-(null, 'microfono', '25000');
+insert into articulos(cod_art, nombre, precio, fabricante) values
+(null, 'monitor', '100000', '2'),
+(null, 'pc', '120000', '3'),
+(null, 'mouse', '12500', '2'),
+(null, 'router', '45600', '4'),
+(null, 'celular', '110000', '1' ),
+(null, 'teclado', '20000', '2'),
+(null, 'motherboard', '56000', '3'),
+(null, 'disco_duro', '43000', '5'),
+(null, 'auriculares', '14300', '5'),
+(null, 'microfono', '25000', '4');
+
+
+
+
+
+select nombre from articulos;
+
+select nombre, precio from articulos;
+
+select nombre from articulos where precio < 20000;
+
+select *from articulos where precio >= 60000 and precio <= 120000;
+
+select nombre, precio/700 from articulos;
+
+select avg(precio) from articulos; 
+
+select avg(precio) from articulos where fabricante = 2; 
+
+select count(precio) from articulos where precio >= 18000;
+
